5daf4cc63b962586de49ef3a4f15e91531e3bd21
Ihnatiev <valerij.ignatjev@gmail.com>
2021-08-25 09:04:11 +0300

    edited lint

--- src/departments/departments.model.ts
+++ src/departments/departments.model.ts
@@ -1,14 +1,14 @@
 import { Column, DataType, Model, Table } from "sequelize-typescript";
 
 interface DepartmentCreationAttrs {
-  name: string;
+    name: string;
 }
 
-@Table({ tableName: "departments", createdAt: false, updatedAt: false })
+@Table({tableName: "departments", createdAt: false, updatedAt: false})
 export class Department extends Model<Department, DepartmentCreationAttrs> {
-  @Column({ type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true })
-  dpId: number;
+    @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
+    dpId: number;
 
-  @Column({ type: DataType.STRING, allowNull: false })
-  dpName: string;
+    @Column({type: DataType.STRING, allowNull: false})
+    dpName: string;
 }
--- src/employees/employees.model.ts
+++ src/employees/employees.model.ts
@@ -3,31 +3,31 @@ import { User } from "../users/users.model";
 import { Department } from "../departments/departments.model";
 
 interface EmployeeCreationAttrs {
-  name: string;
-  active: boolean;
-  dpId: number;
-  userId: string;
+    name: string;
+    active: boolean;
+    dpId: number;
+    userId: string;
 }
 
-@Table({ tableName: "employees", createdAt: false, updatedAt: false })
+@Table({tableName: "employees", createdAt: false, updatedAt: false})
 export class Employee extends Model<Employee, EmployeeCreationAttrs> {
-  @Column({ type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true })
-  id: number;
+    @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
+    id: number;
 
-  @Column({ type: DataType.STRING, allowNull: false })
-  name: string;
+    @Column({type: DataType.STRING, allowNull: false})
+    name: string;
 
-  @Column({ type: DataType.BOOLEAN, allowNull: false })
-  active: boolean;
+    @Column({type: DataType.BOOLEAN, allowNull: false})
+    active: boolean;
 
-  @ForeignKey(() => Department)
-  @Column({ type: DataType.INTEGER })
-  dpId: number;
+    @ForeignKey(() => Department)
+    @Column({type: DataType.INTEGER})
+    dpId: number;
 
-  @ForeignKey(() => User)
-  @Column({ type: DataType.UUID })
-  userId: string;
+    @ForeignKey(() => User)
+    @Column({type: DataType.UUID})
+    userId: string;
 
-  @BelongsTo(() => User)
-  author: User;
+    @BelongsTo(() => User)
+    author: User;
 }
